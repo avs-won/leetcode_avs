0952be64fc23f028b824604359185fefb9778354
SION77 <ds4myu@naver.com>
2025-08-12 22:14:15 +0900

    12주차 3문제(완)

--- leetcode.py
+++ leetcode.py
@@ -606,7 +606,7 @@ print(sol.hasCycle(node1))
 #개인
 #https://leetcode.com/problems/jump-game-ii/submissions/?envType=study-plan-v2&envId=top-interview-150
 
-
+""" 
 class Solution(object):
     def jump(self, nums):
         result=0
@@ -623,5 +623,43 @@ class Solution(object):
     
 sol=Solution()
 print(sol.jump([2,3,1,1,4]))
-
+ """
         
+#https://leetcode.com/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+class Solution(object):
+    # def maxDepth(self, root):
+    #     if not root:
+    #         return 0
+    #     return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
+    
+    def maxDepth(self,root):
+        if not root:
+            return 0
+        level=0
+        q=deque([root])
+        while q:
+            
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                
+            level+=1
+        return level
+    
+    
+    
+    
+
